5ae7acff9f51c9baff6c0c695e40ea4f8a424250
Manav Malhotra <manavmalhotra173@gmail.com>
2023-10-07 12:22:10 +0530

    Create lab1q2.c

---
+++ Lab 1/lab1q2.c
@@ -0,0 +1,19 @@
+// Program to extract each byte from a given number and store them in separate character variables and print the content of those variables
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int num = 0x1A23FEB2;
+    unsigned char ptr = (unsigned char)&num;
+
+    unsigned char b0 = (num >> 24) & 0xFF;
+    unsigned char b1 = (num >> 16) & 0xFF;
+    unsigned char b2 = (num >> 8) & 0xFF;
+    unsigned char b3 = num & 0xFF;
+
+    printf ("The bytes are: ");
+    printf ("%X %X %X %X\n", b0, b1, b2, b3);
+
+    return 0;
+}
