205a90aeaa0f7261ea54d7d90be70d9a203020ce
Manav Malhotra <manavmalhotra173@gmail.com>
2023-10-12 19:08:30 +0530

    Create lab1q6.c

---
+++ Lab 1/lab1q6.c
@@ -0,0 +1,30 @@
+// Program to to convert Little Endian to Big Endian format by using a suitable user defined function for it
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void littletobig(unsigned char *ptr, unsigned int num)
+{
+    for (int i = 0; i < (sizeof(num))/2; i++)
+    {
+        unsigned char temp = ptr[i];
+        ptr[i] = ptr[sizeof(num) - i - 1];
+        ptr[sizeof(num) - i - 1] = temp;
+    }
+
+    printf ("The number after conversion from little endian to big endian is: 0x%X%X%X%X\n", *ptr, *(ptr+1), *(ptr+2), *(ptr+3));
+}
+
+
+int main()
+{
+    unsigned int num = 0x1A23FEB2;
+    unsigned char *ptr = (unsigned char*)&num;
+
+    printf ("The number in little endian format is: 0x%X%X%X%X\n", *ptr, *(ptr+1), *(ptr+2), *(ptr+3));
+
+    littletobig(ptr, num);
+
+    return 0;
+}
