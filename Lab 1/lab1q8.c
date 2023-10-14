6c691650fcf3f11657a4cc8b4a5d949fb3406d94
Manav Malhotra <manavmalhotra173@gmail.com>
2023-10-14 11:43:49 +0530

    Create lab1q8.c

---
+++ Lab 1/lab1q8.c
@@ -0,0 +1,31 @@
+/*Write a program to modify the above program to regenerate the original
+number from the structure members. Display the number. */
+
+#include <stdio.h>
+
+struct pkt
+{
+    char ch1;
+    char ch2[2];
+    char ch3;
+};
+
+int main()
+{
+    struct pkt myPacket;
+
+    myPacket.ch1 = 'A';
+    myPacket.ch2[0] = 'B';
+    myPacket.ch2[1] = 'C';
+    myPacket.ch3 = 'D';
+
+    unsigned int regeneratedValue = 0;
+    regeneratedValue |= ((unsigned int)myPacket.ch1 & 0xFF);
+    regeneratedValue |= ((unsigned int)myPacket.ch2[0] << 8) & 0xFF00;
+    regeneratedValue |= ((unsigned int)myPacket.ch2[1] << 16) & 0xFF0000;
+    regeneratedValue |= ((unsigned int)myPacket.ch3 << 24) & 0xFF000000;
+
+    printf("Regenerated Number: %X\n", regeneratedValue);
+
+    return 0;
+}
